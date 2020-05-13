48fbbe8005e065ee1a0df7cb68924ac76d024b8d
kamilatrhea <k.wojnowski@rheagroup.com>
2020-05-13 09:26:26 +0200

    Cleaning first part; fixes #66

--- IntegrationTestSuite/WebservicesIntegrationTests/Tests/EngineeringModel/Iteration/IterationTestFixture.cs
+++ IntegrationTestSuite/WebservicesIntegrationTests/Tests/EngineeringModel/Iteration/IterationTestFixture.cs
@@ -364,13 +364,13 @@ namespace WebservicesIntegrationTests
                 jArray.Single(x => (string)x[PropertyNames.Iid] == "9ec982e4-ef72-4953-aa85-b158a95d8d56");
             Assert.AreEqual(3, (int)engineeringModel[PropertyNames.RevisionNumber]);
 
-             var expectedIteration = new string[]
+            expectedIterations = new string[]
             {
                 "699da906-d22e-4969-b606-1fcb4bf5affd"
             };
             iterationsArray = (JArray)engineeringModel[PropertyNames.Iteration];
             iterations = iterationsArray.Select(x => (string)x).ToList();
-            CollectionAssert.AreEquivalent(expectedIteration, iterations);
+            CollectionAssert.AreEquivalent(expectedIterations, iterations);
 
             var deletedIterationUri =
                 new Uri(string.Format(UriFormat, this.Settings.Hostname,
