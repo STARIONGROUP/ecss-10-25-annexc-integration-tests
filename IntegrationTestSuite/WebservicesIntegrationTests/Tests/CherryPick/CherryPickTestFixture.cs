37cedd3e815ff08d85ceade4107d57232560b663
samatstarion <s.gerene@stariongroup.eu>
2025-12-07 15:10:50 +0100

    [Update] copyright statement

--- IntegrationTestSuite/WebservicesIntegrationTests/Tests/CherryPick/CherryPickTestFixture.cs
+++ IntegrationTestSuite/WebservicesIntegrationTests/Tests/CherryPick/CherryPickTestFixture.cs
@@ -1,12 +1,20 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="CherryPickTestFixture.cs" company="Starion Group S.A.">
-// 
-//    Copyright (c) 2023 Starion Group S.A.
-// 
-//    Authors: Sam Gerené, Antoine Théate, Jaime Bernar, Omar Elebiary
-// 
-//    This file is part of AP1067 application
-//    The  AP1067 application is a Web Application implementation of ECSS-E-TM-10-25 for AP1067.
+//
+//   Copyright 2016-2025 Starion Group S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
